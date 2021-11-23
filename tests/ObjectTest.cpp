b51eea23374e57668337b22dc05244a4db1ec58f
Tousif Zaman <tousif.zaman@sensible4.fi>
2021-11-23 21:46:51 +0200

    Rename tests.cc to ObjectTest.cpp and fix unit tests

--- tests/ObjectTest.cpp
+++ tests/ObjectTest.cpp
@@ -5,25 +5,27 @@
 TEST(CreateBaseClassObjectAndTestMembers, BaseClassTest)
 {
     // Arrange
+    sf::Vector2<float> position;
+    position.x = 5.0f;
+    position.y = 6.0f;
+
     sf::CircleShape hitbox;
     hitbox.setRadius(10);
     hitbox.setPointCount(5);
     
     sf::Texture sprite;
+    // NOTE Dimension declared as sf::Vector2u which is the type returned by sf::Texture::getSize()
     sf::Vector2u dim;
     dim.x = 2U;
     dim.y = 5U;
     sprite.create(dim.x, dim.y);
-
-    sf::Vector2<unsigned int> position;
-    position.x = 5U;
-    position.y = 6U;
     
     // Act
     td::Object obj(position, hitbox, sprite);
 
     // Assert
-    EXPECT_EQ(obj.getPosition(), position);
+    EXPECT_FLOAT_EQ(obj.getPosition().x, position.x);
+    EXPECT_FLOAT_EQ(obj.getPosition().y, position.y);
     EXPECT_EQ(obj.getHitbox().getRadius(), hitbox.getRadius());
     EXPECT_EQ(obj.getHitbox().getPointCount(), hitbox.getPointCount());
     EXPECT_EQ(obj.getSprite().getSize(), dim);
