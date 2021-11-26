edd029e9de425b672f4c334aafe75a57d8557a42
juliusj12 <julius.jarvilinna@gmail.com>
2021-11-26 16:28:49 +0200

    Change the foulder of basic_tower.cpp

--- src/logic/towers/basic_tower.cpp
+++ src/logic/towers/basic_tower.cpp
@@ -3,7 +3,7 @@
 
 sf::Vector2<float> position = sf::Vector2<float>(5.0f,5.0f); //putted some random numbers for now
 
-sf::CircleShape hitbox = sf::CircleShape(1.0f, 30UL); //radius, and pointCount
+sf::CircleShape hitbox = sf::CircleShape(1.0f, 30UL); //parameters radius and pointCount
 
 sf::Texture sprite = sf::Texture();   //picture of the tower to here
 
@@ -14,4 +14,3 @@ unsigned int attack_speed = 10;  //can adjust these later
 float range = 10.0f;   
 
 td::Tower basic_tower(position, hitbox, sprite, rotation_angle, attack_speed, range);
-
