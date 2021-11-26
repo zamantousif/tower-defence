7968aac827d87c086725b9404c4957b0719874f6
Tousif Zaman <tousif.zaman@sensible4.fi>
2021-11-26 04:31:44 +0200

    Add implementation for Projectile class

---
+++ include/Projectile.hpp
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <Object.hpp>
+
+namespace td
+{
+    class Projectile : public Object
+    {
+        public:
+            /// \brief Projectile constructor
+            /// \param position             Position of the projectile
+            /// \param hitbox               Shape representing the region occupied by the projectile
+            /// \param sprite               Texture of the projectile
+            /// \param rotation_angle       Orientation, in radians of the projectile
+            /// \param damage               Damage % of the projectile
+            /// \param is_armor_piercing    Status of projectile armor
+            /// \param enemy_pierced_count  Number of enemies pierced by the projectile before disappearing
+            Projectile(sf::Vector2<float> position, sf::CircleShape hitbox, sf::Texture sprite, float rotation_angle, unsigned int damage, 
+            bool is_armor_piercing, unsigned int enemy_pierced_count);
+
+            /// \brief Get the position of the projectile
+            /// \return Position of the projectile
+            virtual sf::Vector2<float> getPosition();
+
+            /// \brief Set the rotation of the projectile
+            /// \param angle    Orientation, in radians of the projectile
+            virtual void setRotation(float angle);
+
+            /// \brief Get the orientation of the projectile
+            /// \return Orientation, in radians of the projectile
+            virtual float getRotation();
+
+            /// \brief Get the damage % of the projectile
+            /// \return Damage % of the projectile
+            unsigned int getDamage();
+
+            /// \brief Get the armor piercing status of the projectile
+            /// \return True if projectile has armor piercing active, false otherwise
+            bool isArmorPiercing();
+
+            /// \brief Set the number of enemies pierced by the projectile
+            /// \param count    Number of enemies pierced by the projectile
+            void setEnemyPiercedCount(unsigned int count);
+
+            /// \brief Get the number of enemies pierced by the projectile
+            /// \return Number of enemies pierced by the projectile
+            unsigned int getEnemyPiercedCount();
+
+        protected:
+            unsigned int damage_;                   ///< Damage % of the projectile
+            bool is_armor_piercing_;                ///< Armor piercing status of the projectile
+            unsigned int enemy_pierced_count_;      ///< Number of enemies pierced by the projectile before disappearing
+    };
+}
---
+++ src/Projectile.cpp
@@ -0,0 +1,44 @@
+#include "Projectile.hpp"
+
+namespace td
+{
+    Projectile::Projectile(sf::Vector2<float> position, sf::CircleShape hitbox, sf::Texture sprite, float rotation_angle, unsigned int damage, 
+    bool is_armor_piercing, unsigned int enemy_pierced_count) : Object(position, hitbox, sprite, rotation_angle), damage_(damage), 
+        is_armor_piercing_(is_armor_piercing), enemy_pierced_count_(enemy_pierced_count) {}
+    
+    sf::Vector2<float> Projectile::getPosition()
+    {
+        return position_;
+    }
+
+    void Projectile::setRotation(float angle)
+    {
+        rotation_angle_ = angle;
+    }
+
+    float Projectile::getRotation()
+    {
+        return rotation_angle_;
+    }
+
+    unsigned int Projectile::getDamage()
+    {
+        return damage_;
+    }
+
+    bool Projectile::isArmorPiercing()
+    {
+        return is_armor_piercing_;
+    }
+
+    void Projectile::setEnemyPiercedCount(unsigned int count)
+    {
+        enemy_pierced_count_ = count;
+    }
+
+    unsigned int Projectile::getEnemyPiercedCount()
+    {
+        return enemy_pierced_count_;
+    }
+
+}
