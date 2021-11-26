0a3ce1ebdfd14b64d5e82b18425595364e9b62dd
Tousif Zaman <tousif.zaman@sensible4.fi>
2021-11-26 02:30:37 +0200

    Add public methods setRotation(), getRotation() and rename variable names as per google style guide

--- include/Tower.hpp
+++ include/Tower.hpp
@@ -14,14 +14,14 @@ namespace td
             /// \param position     Position of the tower
             /// \param hitbox       Shape around the tower where damage is effective
             /// \param sprite       Texture of the tower
-            /// \param attackSpeed  Attack speed of the tower
+            /// \param attack_speed  Attack speed of the tower
             /// \param range        Radius of the shape representing damage area of the tower 
-            Tower(sf::Vector2<float> position, sf::CircleShape hitbox, sf::Texture sprite, unsigned int attackSpeed, float range);
+            Tower(sf::Vector2<float> position, sf::CircleShape hitbox, sf::Texture sprite, float rotation_angle, unsigned int attack_speed, float range);
             
             /// \brief Tower constructor
             /// \param position     Position of the tower
-            /// \param attackSpeed  Attack speed of the tower
-            explicit Tower(sf::Vector2<float> position, unsigned int attackSpeed);
+            /// \param attack_speed  Attack speed of the tower
+            explicit Tower(sf::Vector2<float> position, float rotation_angle, unsigned int attack_speed);
 
             /// \brief Get the position of the tower
             /// \return Position of the tower
@@ -30,6 +30,14 @@ namespace td
             /// \brief Get the shape around the tower where damage is effective
             /// \return Shape around the tower where damage is effective
             virtual sf::CircleShape getHitbox();
+
+            /// \brief Set the rotation of the tower
+            /// \param angle    Orientation of the tower
+            virtual void setRotation(float angle);
+
+            /// \brief Get the orientation of the tower
+            /// \return Orientation, in radians of the tower
+            virtual float getRotation();
 
             /// \brief Get the attack speed of the tower
             /// \return Attack speed of the tower
@@ -40,7 +48,7 @@ namespace td
             virtual float getRange();
 
         protected:
-            unsigned int attackSpeed_;  ///< Attack speed of the tower
+            unsigned int attack_speed_;  ///< Attack speed of the tower
             float range_;               ///< Radius of the shape representing damage area of the tower
     };
 }
--- src/Tower.cpp
+++ src/Tower.cpp
@@ -2,11 +2,11 @@
 
 namespace td
 {
-    Tower::Tower(sf::Vector2<float> position, sf::CircleShape hitbox, sf::Texture sprite, unsigned int attackSpeed, float range) : 
-        Object(position, hitbox, sprite), attackSpeed_(attackSpeed), range_(range) {}
+    Tower::Tower(sf::Vector2<float> position, sf::CircleShape hitbox, sf::Texture sprite, float rotation_angle, unsigned int attack_speed, float range) : 
+        Object(position, hitbox, sprite, rotation_angle), attack_speed_(attack_speed), range_(range) {}
 
-    Tower::Tower(sf::Vector2<float> position, unsigned int attackSpeed) : 
-        Object(position, sf::CircleShape(1.0f, 30UL), sf::Texture()), attackSpeed_(attackSpeed), range_(1.0) {}
+    Tower::Tower(sf::Vector2<float> position, float rotation_angle, unsigned int attack_speed) : 
+        Object(position, sf::CircleShape(1.0f, 30UL), sf::Texture(), rotation_angle), attack_speed_(attack_speed), range_(1.0) {}
 
     sf::Vector2<float> Tower::getPosition()
     {
@@ -17,15 +17,24 @@ namespace td
     {
         return hitbox_;
     }
+    
+    void Tower::setRotation(float angle)
+    {
+        rotation_angle_ = angle;
+    }
+
+    float Tower::getRotation()
+    {
+        return rotation_angle_;
+    }
 
     unsigned int Tower::getAttackSpeed()
     {
-        return attackSpeed_;
+        return attack_speed_;
     }
 
     float Tower::getRange()
     {
         return range_;
     }
-
 }
