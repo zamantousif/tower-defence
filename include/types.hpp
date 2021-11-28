9b7b735703efb559596fddf26d2cd1b6245e9896
Konsta Kemppainen <konsta.kemppainen@aalto.fi>
2021-11-28 19:26:10 +0200

    Add a BlockedRegion type

--- include/types.hpp
+++ include/types.hpp
@@ -5,4 +5,5 @@
 /// \brief Define types useful to our game.
 namespace td::types {
   using Position = sf::Vector2f; ///< The type that stores the position of any game object.
+  using BlockedRegion = sf::ConvexShape; ///< Represents a region where towers cannot be placed.
 }
