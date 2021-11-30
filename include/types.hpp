41bc3c2cde2061929b5a2c589c7c3e0a7eee6266
Tousif Zaman <tousif.zaman@sensible4.fi>
2021-11-30 18:16:25 +0200

    Add clang-format, VScode settings files to incorporate google style guide
    clang-formatter is added to use Google style guide in the project source files, invoke it using Ctrl+Shift+I

--- include/types.hpp
+++ include/types.hpp
@@ -4,5 +4,6 @@
 
 /// \brief Define types useful to our game.
 namespace td::types {
-  using Position = sf::Vector2f; ///< The type that stores the position of any game object.
+using Position =
+    sf::Vector2f;  ///< The type that stores the position of any game object.
 }
