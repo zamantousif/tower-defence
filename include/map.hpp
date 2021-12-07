be02e1e4c4d4dd25600a85df8762c8492008dd1c
Konsta Kemppainen <konsta.kemppainen@aalto.fi>
2021-12-07 19:03:44 +0200

    Fix formatting

--- include/map.hpp
+++ include/map.hpp
@@ -7,23 +7,25 @@
 #include "types.hpp"
 
 namespace td {
-  class Map {
-   public:
-    Map(const std::string& background_image_path, std::vector<td::types::Position> enemy_path, std::vector<td::types::BlockedRegion> blocked_regions);
-    
-    const std::string& getBackgroundImagePath();
+class Map {
+ public:
+  Map(const std::string& background_image_path,
+      std::vector<td::types::Position> enemy_path,
+      std::vector<td::types::BlockedRegion> blocked_regions);
 
-    std::vector<td::types::Position>& getEnemyPath();
-    const std::vector<td::types::Position>& getEnemyPath() const;
-    
-    std::vector<td::types::BlockedRegion>& getBlockedRegions();
-    const std::vector<td::types::BlockedRegion>& getBlockedRegions() const;
-    
-    static Map* LoadFromFile(const std::string& file_name);
+  const std::string& getBackgroundImagePath();
 
-   private:
-    const std::string& background_image_path_;
-    std::vector<td::types::Position> enemy_path_;
-    std::vector<td::types::BlockedRegion> blocked_regions_;
-  };
-}
+  std::vector<td::types::Position>& getEnemyPath();
+  const std::vector<td::types::Position>& getEnemyPath() const;
+
+  std::vector<td::types::BlockedRegion>& getBlockedRegions();
+  const std::vector<td::types::BlockedRegion>& getBlockedRegions() const;
+
+  static Map* LoadFromFile(const std::string& file_name);
+
+ private:
+  const std::string& background_image_path_;
+  std::vector<td::types::Position> enemy_path_;
+  std::vector<td::types::BlockedRegion> blocked_regions_;
+};
+}  // namespace td
