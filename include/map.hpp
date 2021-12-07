fa96a0518903896bd69412c9069ce4c0a93742ef
Konsta Kemppainen <konsta.kemppainen@aalto.fi>
2021-12-07 19:09:20 +0200

    Add documentation

--- include/map.hpp
+++ include/map.hpp
@@ -9,18 +9,29 @@
 namespace td {
 class Map {
  public:
+  /// \param background_image_path Path to the map background
+  /// \param enemy_path Defines the path that the enemies take
+  /// \param blocked_regions Defines regions that towers can't be placed on
   Map(const std::string& background_image_path,
       std::vector<td::types::Position> enemy_path,
       std::vector<td::types::BlockedRegion> blocked_regions);
 
+  /// \return A path to the background image
   const std::string& getBackgroundImagePath();
 
+  /// \return A reference to the enemy path
   std::vector<td::types::Position>& getEnemyPath();
+  /// \return A const reference to the enemy path
   const std::vector<td::types::Position>& getEnemyPath() const;
 
+  /// \return A reference to a vector of the blocked regions
   std::vector<td::types::BlockedRegion>& getBlockedRegions();
+  /// \return A const reference to a vector of the blocked regions
   const std::vector<td::types::BlockedRegion>& getBlockedRegions() const;
 
+  /// \param file_name Path to the JSON file to load the Map from
+  /// 
+  /// \return A pointer to a Map that was loaded from the given file
   static Map* LoadFromFile(const std::string& file_name);
 
  private:
