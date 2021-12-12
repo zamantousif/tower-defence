1b145882e727590515c4f7cc1cd152317915b476
Tousif Zaman <tousif.zaman@sensible4.fi>
2021-12-12 11:50:58 +0200

    Fix comments

--- tests/collision_test.cpp
+++ tests/collision_test.cpp
@@ -13,7 +13,7 @@ class CollisionTest : public ::testing::Test {
 };
 
 TEST_F(CollisionTest, EuclideanDistance) {
-  // Act
+  // Arrange
   td::types::Position p, A, B, C, D, O;
   p = sf::Vector2f(0.0f, 1.0f);
   A = sf::Vector2f(0.0f, 1.0f);
@@ -21,8 +21,7 @@ TEST_F(CollisionTest, EuclideanDistance) {
   C = sf::Vector2f(1.0f, 2.0f);
   D = sf::Vector2f(0.0f, 2.0f);
   O = sf::Vector2f(0.0f, 0.0f);
-
-  // Arrange
+  // Act
   double dist_edgeAB = td::EuclideanDistance(A, B);
   double dist_edgeBC = td::EuclideanDistance(B, C);
   double dist_edgeCD = td::EuclideanDistance(C, D);
@@ -30,7 +29,6 @@ TEST_F(CollisionTest, EuclideanDistance) {
   double dist_edgeOD = td::EuclideanDistance(O, D);
   double dist_edgeAC = td::EuclideanDistance(A, C);
   double dist_edgeDB = td::EuclideanDistance(D, B);
-
   // Assert
   EXPECT_FLOAT_EQ(dist_edgeAB, 1.0);
   EXPECT_FLOAT_EQ(dist_edgeBC, 1.0);
@@ -42,7 +40,7 @@ TEST_F(CollisionTest, EuclideanDistance) {
 }
 
 TEST_F(CollisionTest, IsPointBetween) {
-  // Act
+  // Arrange
   td::types::Position p, A, B, C, D, O;
   p = sf::Vector2f(0.0f, 1.0f);
   A = sf::Vector2f(0.0f, 1.0f);
@@ -50,14 +48,12 @@ TEST_F(CollisionTest, IsPointBetween) {
   C = sf::Vector2f(1.0f, 2.0f);
   D = sf::Vector2f(0.0f, 2.0f);
   O = sf::Vector2f(0.0f, 0.0f);
-
-  // Arrange
+  // Act
   bool is_p_on_edgeAB = td::IsPointBetween(A, p, B);
   bool is_p_on_edgeBC = td::IsPointBetween(B, p, C);
   bool is_p_on_edgeCD = td::IsPointBetween(C, p, D);
   bool is_p_on_edgeDA = td::IsPointBetween(D, p, A);
   bool is_p_on_edgeOD = td::IsPointBetween(O, p, D);
-
   // Assert
   EXPECT_TRUE(is_p_on_edgeAB);
   EXPECT_FALSE(is_p_on_edgeBC);
@@ -167,7 +163,7 @@ TEST_F(CollisionTest, IsCircleIntersectingPolygonEdge) {
       td::IsCircleIntersectingPolygonEdge(p1, r, edge_DE);
   bool is_circle_intersecting_EA =
       td::IsCircleIntersectingPolygonEdge(p1, r, edge_EA);
-  // Arrange
+  // Assert
   EXPECT_TRUE(is_circle_intersecting_AB);
   EXPECT_FALSE(is_circle_intersecting_BC);
   EXPECT_FALSE(is_circle_intersecting_CD);
@@ -230,7 +226,6 @@ TEST_F(CollisionTest, IsCircleCollidingWithPolygon) {
   edges.emplace_back(edge_BC);
   edges.emplace_back(edge_CD);
   edges.emplace_back(edge_DA);
-
   // Act
   bool is_circle_intersecting_AB =
       td::IsCircleIntersectingPolygonEdge(p, r, edge_AB);
@@ -243,7 +238,6 @@ TEST_F(CollisionTest, IsCircleCollidingWithPolygon) {
   bool is_circle_inside_polygon = td::IsCircleCenterInsidePolygon(p, edges);
   bool is_circle_colliding_with_polygon =
       td::IsCircleCollidingWithPolygon(p, r, polygon);
-
   // Assert
   EXPECT_TRUE(is_circle_intersecting_AB);
   EXPECT_FALSE(is_circle_intersecting_BC);
